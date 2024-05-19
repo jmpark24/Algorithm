e8c9b98e098b93c90a031c913a35b38b5af6e628
jmpark <148678966+jmpark24@users.noreply.github.com>
2024-05-19 15:02:17 +0900

    [Bronze V] Title: A+B - 7, Time: 116 ms, Memory: 9344 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.js
@@ -0,0 +1,8 @@
+const input = require('fs').readFileSync('dev/stdin').toString().split('\n'); // 여러 줄에 걸쳐 입력을 받았기에 \n 으로 쪼개야 함.
+
+let result = '';
+for (let i = 1; i <= input[0]; i++) {
+  let tmp = input[i].split(' ');
+  result += `Case #${i}: ` + (parseInt(tmp[0]) + parseInt(tmp[1])) + '\n';
+}
+console.log(result);
