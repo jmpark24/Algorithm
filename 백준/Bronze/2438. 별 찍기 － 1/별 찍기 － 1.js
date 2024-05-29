07f2683e2e336ecf8a23fef3d9a0ba1a191c8fbf
jmpark <148678966+jmpark24@users.noreply.github.com>
2024-05-29 13:23:30 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 116 ms, Memory: 9480 KB -BaekjoonHub

--- 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.js
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.js
@@ -1,12 +1,11 @@
-const input = require('fs').readFileSync('dev/stdin').toString().split('\n'); // 여러 줄에 걸쳐 입력을 받았기에 \n 으로 쪼개야 함.
+let fs = require("fs");
+let input = fs.readFileSync("dev/stdin").toString().split(" ").map(Number);
 
-let output = '';
-
-for (let i = 1; i <= parseInt(input[0]); i++) {
+let result = "";
+for (let i = 1; i <= input[0]; i++) {
   for (let j = 1; j <= i; j++) {
-    output += '*';
+    result += "*";
   }
-  output += '\n';
+  result += "\n";
 }
-
-console.log(output);
+console.log(result);
