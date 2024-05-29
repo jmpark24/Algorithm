b4651a39b25e4d72c4056a27c253e409f27fe269
jmpark <148678966+jmpark24@users.noreply.github.com>
2024-05-29 13:35:33 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 1280 ms, Memory: 161384 KB -BaekjoonHub

--- 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.js
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.js
@@ -1,9 +1,13 @@
-const input = require('fs').readFileSync('dev/stdin').toString().split('\n'); // 여러 줄에 걸쳐 입력을 받았기에 \n 으로 쪼개야 함.
+let fs = require("fs");
+let input = fs.readFileSync("dev/stdin").toString().split("\n");
 
-let answer = '';
-for (let i = 1; i <= input[0]; i++) {
-  let tmp = input[i].split(' ');
-  answer += parseInt(tmp[0]) + parseInt(tmp[1]) + '\n';
+let testCase = Number(input[0]);
+let answer = "";
+
+for (let i = 1; i <= testCase; i++) {
+  let data = input[i].split(" ");
+  let a = Number(data[0]);
+  let b = Number(data[1]);
+  answer += a + b + "\n";
 }
-
 console.log(answer);
