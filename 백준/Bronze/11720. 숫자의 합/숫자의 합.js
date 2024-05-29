6aed7dc361f5e32be0dee3134f88b69dacd437a6
jmpark <148678966+jmpark24@users.noreply.github.com>
2024-05-29 15:18:46 +0900

    [Bronze IV] Title: 숫자의 합, Time: 128 ms, Memory: 9588 KB -BaekjoonHub

--- 백준/Bronze/11720. 숫자의 합/숫자의 합.js
+++ 백준/Bronze/11720. 숫자의 합/숫자의 합.js
@@ -1,11 +1,12 @@
-const input = require('fs').readFileSync('dev/stdin').toString().split('\n');
+let fs = require("fs");
+let input = fs.readFileSync("dev/stdin").toString().split("\n");
 
-const count = parseInt(input[0]);
-
-const arr = input[1].split('').map(Number);
+let testCases = Number(input[0]);
+let numbers = input[1].split("").map(Number);
 
 let sum = 0;
-for (let i = 0; i < count; i++) {
-  sum += arr[i];
+
+for (let i = 0; i < testCases; i++) {
+  sum += numbers[i];
 }
 console.log(sum);
